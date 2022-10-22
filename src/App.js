909125c5a9f4a69894e09c27217fccc2256ca317
igorGavr <ragamuffinrogi@gmail.com>
2022-10-22 16:14:07 +0300

    pizza

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/Header/Header";
+import {Categories} from "./components/Categories/Categories";
+import {Sort} from "./components/Sort/Sort";
+import {PizzaBlock} from "./components/PizzaBlock/PizzaBlock";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <Header/>
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories/>
+                            <Sort/>
+                        </div>
+                        <h2 className="content__title">Все пиццы</h2>
+                        <div className="content__items">
+                            <PizzaBlock title="Mexican" price="200"/>
+                            <PizzaBlock title="Hawai" price="230"/>
+                            <PizzaBlock title="Cheesy" price="250"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
